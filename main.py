942bb6e8f1c1691d86301787567cc7401ca1db0a
nayfaan <luknnn129@gmail.com>
2023-11-17 05:32:28 -0800

    v.1.0. - functional

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import tkinter as tk
-import keyboard
+from numpy.random import normal as npRand
+
+# import keyboard
 from pynput import mouse
 from pynput import keyboard as kb
 
+import multiprocessing
+
 from AppKit import NSWorkspace
 from os import system
+from time import sleep
 
 
 def validateInt(inStr, acttyp):
@@ -35,17 +40,37 @@ def newCoordinate(coordinates_list_frame):
     YCoord["validatecommand"] = (YCoord.register(validateInt), "%P", "%d")
     YCoord.grid(row=0, column=3)
 
-    locate_btn = tk.Button(master=newCoordFrame, text="Locate")
-    locate_btn.bind(
-        "<Button-1>",
-        lambda event: locate_btn_press(target_window, newCoordFrame),
-    )
-    locate_btn.grid(row=0, column=4)
+    tk.Button(
+        master=newCoordFrame,
+        text="Locate",
+        command=lambda: locate_btn_press(newCoordFrame),
+    ).grid(row=0, column=4)
 
     return newCoordFrame
 
 
+def recheck_target_window():
+    target_window_name = target_window.get()
+    opened_windows = ["(None)"] + [
+        apps["NSApplicationName"]
+        for apps in NSWorkspace.sharedWorkspace().launchedApplications()
+    ]
+    if not target_window_name in opened_windows:
+        target_window.set("(None)")
+
+    window_list["menu"].delete(0, "end")
+    for window in opened_windows:
+        window_list["menu"].add_command(
+            label=window, command=tk._setit(target_window, window)
+        )
+
+
 def target_window_into_focus(target_window):
+    if not target_window == "(None)":
+        try:
+            recheck_target_window()
+        except:
+            pass
     if not target_window == "(None)":
         system(
             '/usr/bin/osascript -e \'tell app "Finder" to set frontmost of process "'
@@ -78,7 +103,7 @@ def move_listener(x, y, x_entry, y_entry):
     tk_set_text(y_entry, round(y))
 
 
-def locate_btn_press(target_window, CoordFrame):
+def locate_btn_press(CoordFrame):
     CoordFrameList = CoordFrame.winfo_children()
 
     CoordFrameList[1].configure(highlightbackground="red", highlightcolor="red")
@@ -95,7 +120,6 @@ def locate_btn_press(target_window, CoordFrame):
 
 
 # def hotkey_press_listener(key):
-#     # print(key)
 #     try:
 #         print("alphanumeric key {0} pressed".format(key.char))
 #     except AttributeError:
@@ -116,61 +140,246 @@ def locate_btn_press(target_window, CoordFrame):
 #     hotkey.configure(background=orig_color)
 
 
-def redraw_coordinates(coordinates):
+def redraw_coordinates():
     for widget in coordinates_list_frame.winfo_children():
         widget.pack_forget()
-    for coordinate in coordinates:
+    for coordinate in coordinates_list:
         coordinate.pack()
     for widget in coordinates_list_frame.winfo_children():
         if not bool(widget.winfo_manager()):
             widget.destroy()
 
 
-def redraw_add_minus_buttons(coordinates):
+def redraw_add_minus_buttons():
     add_coordinate.configure(state="active")
     del_coordinate.configure(state="active")
-    if len(coordinates) >= max_coordinates:
+    if len(coordinates_list) >= max_coordinates:
         add_coordinate.configure(state="disabled")
-    if len(coordinates) <= 1:
+    if len(coordinates_list) <= 1:
         del_coordinate.configure(state="disabled")
 
 
-def redraw_mode(coordinates):
-    if len(coordinates) == 1:
+def redraw_mode():
+    if len(coordinates_list) == 1:
         click_mode.configure(text="Mode: single point")
-    elif len(coordinates) == 2:
+    elif len(coordinates_list) == 2:
         click_mode.configure(text="Mode: corner")
-    elif len(coordinates) > 2:
+    elif len(coordinates_list) > 2:
         click_mode.configure(text="Mode: polygon")
     else:
         click_mode.configure(text="Mode: ERROR")
 
 
-def plus_btn_press(coordinates):
-    if len(coordinates) < max_coordinates:
-        coordinates.append(newCoordinate(coordinates_list_frame))
+def plus_btn_press():
+    if len(coordinates_list) < max_coordinates:
+        coordinates_list.append(newCoordinate(coordinates_list_frame))
 
-        redraw_coordinates(coordinates)
-        redraw_mode(coordinates)
-        redraw_add_minus_buttons(coordinates)
+        redraw_coordinates()
+        redraw_mode()
+        redraw_add_minus_buttons()
 
 
-def minus_btn_press(coordinates):
-    if len(coordinates) > 1:
-        coordinates.pop()
+def minus_btn_press():
+    if len(coordinates_list) > 1:
+        coordinates_list.pop()
 
-        redraw_coordinates(coordinates)
-        redraw_mode(coordinates)
-        redraw_add_minus_buttons(coordinates)
+        redraw_coordinates()
+        redraw_mode()
+        redraw_add_minus_buttons()
+
+
+def random(bound, mean=None, sd=None, toInt=True):
+    if len(bound) == 1:
+        return bound[0]
+    elif len(bound) == 2:
+        if bound[0] == bound[1]:
+            return bound[0]
+
+        if not mean:
+            mean = (bound[0] + bound[1]) / 2
+        if not sd:
+            sd = (bound[1] - bound[0]) / 6
+
+        result = npRand(loc=mean, scale=sd, size=1)[0]
+        if toInt:
+            result = round(result)
+        result = max(min(result, bound[1]), bound[0])
+
+        return result
+    else:
+        raise IndexError("Random range size larger than 2")
+
+
+def pyn_click(pos):
+    while not mouse_control.position == (pos[0], pos[1]):
+        mouse_control.position = (pos[0], pos[1])
+    mouse_control.press(mouse.Button.left)
+    sleep(min(max(npRand(loc=0.09, scale=0.013, size=1)[0], 0), 0.15))
+    mouse_control.release(mouse.Button.left)
+
+
+def click(coordinates_numbers, mean=[None, None], sd=[None, None]):
+    target_window_into_focus(target_window_name)
+
+    pos1 = coordinates_numbers[0]
+    try:
+        pos2 = coordinates_numbers[1]
+    except:
+        pos2 = None
+
+    if not pos2:
+        pyn_click(pos1)
+    else:
+        pyn_click(
+            (
+                random([pos1[0], pos2[0]], mean=mean[0], sd=sd[0]),
+                random([pos1[1], pos2[1]], mean=mean[1], sd=sd[1]),
+            )
+        )
+
+
+def disable_all_inputs():
+    start_button.configure(state="disabled")
+    stop_button.configure(state="active")
+    add_coordinate.configure(state="disabled")
+    del_coordinate.configure(state="disabled")
+
+    for coordinate_frame in coordinates_list_frame.winfo_children():
+        coordinate_list = coordinate_frame.winfo_children()
+        coordinate_list[1].configure(state="readonly")
+        coordinate_list[3].configure(state="readonly")
+        coordinate_list[4].configure(state="disabled")
+
+    time_min.configure(state="readonly")
+    time_max.configure(state="readonly")
+
+    window_list.configure(state="disabled")
+
+
+def reenable_all_inputs():
+    stop_button.configure(state="disabled")
+    start_button.configure(state="active")
+    redraw_add_minus_buttons()
+
+    for coordinate_frame in coordinates_list_frame.winfo_children():
+        coordinate_list = coordinate_frame.winfo_children()
+        coordinate_list[1].configure(state="normal")
+        coordinate_list[3].configure(state="normal")
+        coordinate_list[4].configure(state="active")
+
+    time_min.configure(state="normal")
+    time_max.configure(state="normal")
+
+    window_list.configure(state="normal")
 
 
 def start_btn_press():
-    pass
+    disable_all_inputs()
+
+    coordinates_numbers = []
+    for coordinate_frame in coordinates_list_frame.winfo_children():
+        coordinate_list = coordinate_frame.winfo_children()
+        coordinates_numbers.append([coordinate_list[1].get(), coordinate_list[3].get()])
+
+    for ix, _ in enumerate(coordinates_numbers):
+        for iy, __ in enumerate(coordinates_numbers[ix]):
+            if not coordinates_numbers[ix][iy]:
+                stop_btn_press()
+                return None
+            coordinates_numbers[ix][iy] = int(coordinates_numbers[ix][iy])
+        coordinates_numbers[ix] = tuple(coordinates_numbers[ix])
+
+    if len(coordinates_numbers) == 2:
+        coord_mean = [
+            (coordinates_numbers[0][0] + coordinates_numbers[1][0]) / 2,
+            (coordinates_numbers[0][1] + coordinates_numbers[1][1]) / 2,
+        ]
+        coord_sd = [
+            (coordinates_numbers[1][0] - coordinates_numbers[0][0]) / 6,
+            (coordinates_numbers[1][1] - coordinates_numbers[0][1]) / 6,
+        ]
+    else:
+        coord_mean = None
+        coord_sd = None
+
+    time_numbers = [time_min.get(), time_max.get()]
+    if not time_numbers[0] and not time_numbers[1]:
+        stop_btn_press()
+        return None
+    if not time_numbers[0]:
+        time_numbers[0] = time_numbers[1]
+    if not time_numbers[1]:
+        time_numbers[1] = time_numbers[0]
+    time_numbers = [float(time_numbers[0]) / 1000, float(time_numbers[1]) / 1000]
+    time_numbers = tuple(time_numbers)
+
+    if not time_numbers[0] == time_numbers[1]:
+        time_mean = (time_numbers[0] + time_numbers[1]) / 2
+        time_sd = (time_numbers[1] - time_numbers[0]) / 6
+    else:
+        time_mean = None
+        time_sd = None
+
+    manager = multiprocessing.Manager()
+    target_manager_value = manager.Value(str, target_window.get())
+    p1 = multiprocessing.Process(
+        target=click_loop,
+        args=(
+            coordinates_numbers,
+            coord_mean,
+            coord_sd,
+            time_numbers,
+            time_mean,
+            time_sd,
+        ),
+        kwargs={
+            "target_window": target_manager_value,
+        },
+    )
+    p1.start()
+    p1.join()
+    stop_btn_press()
+
+
+def click_loop(
+    coordinates_numbers,
+    coord_mean,
+    coord_sd,
+    time_numbers,
+    time_mean,
+    time_sd,
+    **kwargs
+):
+    global mouse_control
+    mouse_control = mouse.Controller()
+
+    global stop
+    stop = False
+    global listener
+    listener = kb.Listener(on_press=press_listener)
+    listener.start()
+
+    global target_window_name
+    target_window_name = kwargs.get("target_window").value
+
+    mouse_control.position = (coordinates_numbers[0][0], coordinates_numbers[0][1])
+
+    while not stop:
+        print(stop)
+        click(coordinates_numbers, coord_mean, coord_sd)
+        if time_numbers[0] == time_numbers[1]:
+            sleep(time_numbers[0])
+        else:
+            s = random(time_numbers, mean=time_mean, sd=time_sd, toInt=False)
+            sleep(s)
+
 
 def stop_btn_press():
-    pass
+    reenable_all_inputs()
+
 
 def init():
+    # Main frames
     global frame_l
     global frame_r
     frame_l = tk.Frame(master=root)
@@ -178,6 +387,7 @@ def init():
     frame_l.pack(fill=tk.Y, side=tk.LEFT)
     frame_r.pack(fill=tk.Y, side=tk.LEFT)
 
+    # Windows select frame
     window_select_frame = tk.Frame(master=frame_l, borderwidth=10)
 
     tk.Label(
@@ -185,19 +395,22 @@ def init():
         text="Target Window: ",
     ).grid(row=0, column=0)
 
-    open_windows = [
+    opened_windows = [
         apps["NSApplicationName"]
         for apps in NSWorkspace.sharedWorkspace().launchedApplications()
     ]
     global target_window
     target_window = tk.StringVar(value="(None)")
+    global window_list
     window_list = tk.OptionMenu(
-        window_select_frame, target_window, *["(None)"] + open_windows
+        window_select_frame, target_window, *["(None)"] + opened_windows
     )
+    window_list.bind("<Button-1>", lambda event: recheck_target_window())
     window_list.grid(row=0, column=1)
 
     window_select_frame.pack()
 
+    # Timer frame
     timer_frame = tk.Frame(master=frame_l, borderwidth=10)
 
     tk.Label(
@@ -227,6 +440,7 @@ def init():
 
     timer_frame.pack()
 
+    # Area select frame
     area_select_frame = tk.Frame(master=frame_l, borderwidth=10)
 
     tk.Label(
@@ -244,31 +458,29 @@ def init():
     global coordinates_list_frame
     coordinates_list_frame = tk.Frame(master=area_select_frame)
 
-    coordinates = []
-    coordinates.append(newCoordinate(coordinates_list_frame))
+    global coordinates_list
+    coordinates_list = []
+    coordinates_list.append(newCoordinate(coordinates_list_frame))
 
-    redraw_coordinates(coordinates)
+    redraw_coordinates()
 
     coordinates_list_frame.grid(row=2, column=0, columnspan=2)
 
     global del_coordinate
-    del_coordinate = tk.Button(master=area_select_frame, text="–", state="disabled")
-    del_coordinate.bind(
-        "<Button-1>",
-        lambda event, coordinates=coordinates: minus_btn_press(coordinates),
+    del_coordinate = tk.Button(
+        master=area_select_frame, text="–", state="disabled", command=minus_btn_press
     )
     del_coordinate.grid(row=3, column=0, sticky="E")
 
     global add_coordinate
-    add_coordinate = tk.Button(master=area_select_frame, text="+")
-    add_coordinate.bind(
-        "<Button-1>",
-        lambda event, coordinates=coordinates: plus_btn_press(coordinates),
+    add_coordinate = tk.Button(
+        master=area_select_frame, text="+", command=plus_btn_press
     )
     add_coordinate.grid(row=3, column=1, sticky="W")
 
     area_select_frame.pack()
 
+    # # Hotkey select frame
     # hotkey_select_frame = tk.Frame(master=frame_l, borderwidth=10)
 
     # tk.Label(master=hotkey_select_frame, text="Hotkey: ").grid(row=0, column=0)
@@ -284,45 +496,47 @@ def init():
     # )
     # hotkey.grid(row=0, column=1)
 
-    # hotkey_select_btn = tk.Button(master=hotkey_select_frame, text="Select")
-    # hotkey_select_btn.bind(
-    #     "<Button-1>",
-    #     lambda event: hotkey_select_btn_press(),
-    # )
+    # hotkey_select_btn = tk.Button(master=hotkey_select_frame, text="Select", command=hotkey_select_btn_press)
     # hotkey_select_btn.grid(row=0, column=2)
 
     # hotkey_select_frame.pack()
 
+    # Control frame
     control_frame = tk.Frame(master=frame_l, borderwidth=10)
 
-    start_button = tk.Button(master=control_frame, text="START")
-    start_button.bind(
-        "<Button-1>",
-        lambda event: start_btn_press(),
+    global start_button
+    start_button = tk.Button(
+        master=control_frame, text="START", command=start_btn_press
     )
     start_button.grid(row=0, column=0)
-    stop_button = tk.Button(master=control_frame, text="STOP", state="disabled")
-    stop_button.bind(
-        "<Button-1>",
-        lambda event: stop_btn_press(),
+
+    global stop_button
+    stop_button = tk.Button(
+        master=control_frame, text="STOP", state="disabled", command=stop_btn_press
     )
     stop_button.grid(row=0, column=1)
 
     control_frame.pack()
 
 
+def press_listener(key):
+    if key == kb.Key.esc:
+        stop = True
+        return False
+
+
 def run():
     global root
     root = tk.Tk()
+    root.title("Mac Auto Clicker")
+
+    global mouse_control
+    mouse_control = mouse.Controller()
 
     global max_coordinates
     max_coordinates = 2
 
     init()
-
-    # global listener
-    # listener = kb.Listener(on_press=press_listener)
-    # listener.start()
 
     tk.mainloop()
 
